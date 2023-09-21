96188e88f68bb6614f8b5cde06a560f14cdd3a86
kimjungkwang1 <qwer9808@gmail.com>
2023-09-21 17:53:54 +0900

    230921 commit at SSAFY

--- src/Algo_230921/BJ_4195_친구만들기.java
+++ src/Algo_230921/BJ_4195_친구만들기.java
@@ -4,92 +4,97 @@ import java.util.*;
 
 public class BJ_4195_친구만들기 {
 	static int p[];
-	
+
 	static int set(int x) {
-		if(x!=p[x]) {
-			p[x]=set(p[x]);
+		if (x != p[x]) {
+			p[x] = set(p[x]);
 		}
 		return p[x];
 	}
-	
+
 	static void union(int x, int y) {
-		p[set(y)]=set(x);
+		int px = set(x);
+		int py = set(y);
+			if(px != py) {
+				p[py] = px;	
+			}
 	}
-	
+
 	public static void main(String[] args) {
-		
-	ArrayList<String> friendList = new ArrayList<String>();
-	Scanner sc = new Scanner(System.in);
-	int Tcase = sc.nextInt();
-	
-	for(int tc=1; tc<=Tcase; tc++) {
-		int N =sc.nextInt();
-		String leftFriend[] = new String[N];
-		String rightFriend[] = new String[N];
 
-		for(int i=0; i<N; i++) {
-			leftFriend[i]=sc.next();
-			rightFriend[i]=sc.next();
+		Scanner sc = new Scanner(System.in);
+		ArrayList<String> friendList = new ArrayList<String>();
+
+		int Tcase = sc.nextInt();
+
+		for (int tc = 1; tc <= Tcase; tc++) {
+			int N = sc.nextInt();
+			String leftFriend[] = new String[N];
+			String rightFriend[] = new String[N];
+
+			for (int i = 0; i < N; i++) {
+				leftFriend[i] = sc.next();
+				rightFriend[i] = sc.next();
+
+			}
+			for (int i = 0; i < N; i++) {
+				String l = leftFriend[i];
+				String r = rightFriend[i];
+				if (!friendList.contains(l)) {
+					friendList.add(l);
+				}
+				if (!friendList.contains(r)) {
+					friendList.add(r);
+				}
+			}
+			p = new int[2 * N + 1];
+			for (int i = 1; i <= 2 * N; i++) {
+				p[i] = i;
+			}
+
+			HashMap<String,Integer> hMap = new HashMap<>();
+			//		key		value
+			int index = 0;
+			for (int i = 0; i <  N; i++) {
+				String le = leftFriend[i];
+				String ri = rightFriend[i];
+				
+				if(!hMap.containsKey(le)) {
+					hMap.put(le, index++);
+				}
+				if(!hMap.containsKey(ri)) {
+					hMap.put(ri, index++);
+				}
+			}
 			
-		}
-		for(int i=0; i<N; i++) {
-			String l = leftFriend[i];
-			String r = rightFriend[i];
-			if(!friendList.contains(l)) {
-				friendList.add(l);
+			for (int i = 0; i <  N; i++) {
+				String le = leftFriend[i];
+				String ri = rightFriend[i];
+				int x = hMap.get(le);
+				int y = hMap.get(ri);
+				
+				if(x!=y) {
+					union(x,y);
+				}
+
+
+				int cnt[] = new int [2*N+1];
+				int maxi = 0;
+				for(int j=0; j<=2*N ; j++) {
+					cnt[p[j]]++;
+				}
+				for(int j=0; j<=2*N ; j++) {
+					maxi = Math.max(cnt[j], maxi);
+				}
+				System.out.println(maxi);
+				
 			}
-			if(!friendList.contains(r)) {
-				friendList.add(r);
-			}
-		}
-		p = new int [2*N+1];
-		for(int i=1; i<=2*N;i++) {
-			p[i]=i;
-		}
-		for(int i=0; i<N; i++) {
-			String l = leftFriend[i];
-			String r = rightFriend[i];
-			if(p[whereIsMyfriend(l,friendList)]!=p[whereIsMyfriend(r,friendList)]) {
-			union(p[whereIsMyfriend(l,friendList)],whereIsMyfriend(r,friendList));
-			}
-	
-			for(int j=1; j<=2*N; j++) {
-				set(p[j]);
-			}
-			int cnt[] = new int [2*N+1];
-			for(int j=1; j<=2*N; j++) {
-				cnt[p[j]]++;
-			}
-			int maxi = Integer.MIN_VALUE;
-			for(int j=1; j<=2*N;j++) {
-				maxi = Math.max(cnt[j],maxi);
-			}
-			for(int j=1; j<=2*N; j++) {
-				set(p[j]);
-			}
-			set(p[whereIsMyfriend(l,friendList)]);
-			set(p[whereIsMyfriend(r,friendList)]);
-			System.out.println(maxi);
-			System.out.println(Arrays.toString(p));
-		}
-		
 			
 			
 
-			
-			
-	}//tcase
-		
-		
+		} // tcase
+
 	}
-	
-	static int whereIsMyfriend(String a, ArrayList<String> b) {
-		for(int i=0; i<b.size(); i++) {
-			if(b.get(i).equals(a)) {
-				return i+2;
-			}
-		}
-		return -999;
-		
-	}
+
+
 }
---
+++ src/Algo_230921/Sample_Dikstra.java
@@ -0,0 +1,95 @@
+package Algo_230921;
+
+import java.util.*;
+
+public class Sample_Dikstra {
+    static class Node implements Comparable<Node> {
+        int vertex;
+        int weight;
+
+        public Node(int vertex, int weight) {
+            this.vertex = vertex;
+            this.weight = weight;
+        }
+
+        @Override
+        public int compareTo(Node other) {
+            return Integer.compare(this.weight, other.weight);
+        }
+    }
+
+    public static void dijkstra(List<List<Node>> graph, int start) {
+        int numNodes = graph.size();
+        int[] distance = new int[numNodes];
+        Arrays.fill(distance, Integer.MAX_VALUE); // 초기 거리를 무한대로 설정
+        distance[start] = 0; // 시작 노드의 거리는 0으로 설정
+
+        PriorityQueue<Node> pq = new PriorityQueue<>(); // 우선순위 큐 선언
+        pq.offer(new Node(start, 0)); // 시작 노드를 큐에 추가
+
+        while (!pq.isEmpty()) {
+            Node currentNode = pq.poll(); // 우선순위 큐에서 가장 가까운 노드를 가져옴
+            int x = currentNode.vertex; // 현재 노드의 인덱스
+            int y = currentNode.weight; // 현재 노드까지의 거리
+
+            if (y > distance[x]) {
+                continue; // 이미 처리한 노드이거나 더 긴 거리를 가진 경우, 처리하지 않고 건너뜀
+            }
+
+            for (Node neighbor : graph.get(x)) {
+                int newDistance = y + neighbor.weight; // 현재 노드를 거쳐서 이웃 노드에 도달하는 새로운 거리 계산
+                if (newDistance < distance[neighbor.vertex]) {
+                    distance[neighbor.vertex] = newDistance; // 새로운 거리로 업데이트
+                    pq.offer(new Node(neighbor.vertex, newDistance)); // 업데이트된 거리와 함께 이웃 노드를 우선순위 큐에 추가
+                }
+            }
+
+        }
+
+        // 최단 경로 출력
+        for (int i = 0; i < numNodes; i++) {
+            System.out.println("노드 " + i + "까지의 최단 거리: " + distance[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(input);
+        
+        // 노드 개수와 간선 개수 입력
+        int numNodes = scanner.nextInt();
+        int numEdges = scanner.nextInt();
+        
+        List<List<Node>> graph = new ArrayList<>(numNodes);
+
+        for (int i = 0; i < numNodes; i++) {
+            graph.add(new ArrayList<>()); // 인접 리스트 초기화
+        }
+
+        // 그래프 초기화 (노드와 가중치 추가)
+        for (int i = 0; i < numEdges; i++) {
+            int source = scanner.nextInt();
+            int target = scanner.nextInt();
+            int weight = scanner.nextInt();
+            graph.get(source).add(new Node(target, weight));
+        }
+        
+        int startNode = scanner.nextInt(); // 시작 노드 입력
+        scanner.close();
+        
+        dijkstra(graph, startNode); // 시작 노드를 선택하여 다익스트라 알고리즘 실행
+    }
+    
+    static String input = "6 11\r\n" +
+            "0 1 4\r\n" +
+            "0 2 2\r\n" +
+            "0 5 25\r\n" +
+            "1 3 8\r\n" +
+            "1 4 7\r\n" +
+            "2 1 1\r\n" +
+            "2 4 4\r\n" +
+            "3 0 3\r\n" +
+            "3 5 6\r\n" +
+            "4 3 5\r\n" +
+            "4 5 12\r\n"
+            + "0";
+}
